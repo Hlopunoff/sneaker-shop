91e048b8655f55d1901776460e918698340f387e
Vasiliy Khlopunov <khlopunov_v@goldapple.ru>
2024-02-26 21:39:22 +0500

    Добавили шрифты и обнулили проект

--- src/layouts/main-layout/index.js
+++
@@ -1,1 +0,0 @@
-export { default as AppLayoutMain } from './main-layout.vue'
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import { AppLayoutMain } from '../layouts/main-layout'
-
 const routes = [
-  {path: '/', component: AppLayoutMain}
 ]
 
 export const router = createRouter({
