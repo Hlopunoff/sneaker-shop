f5a6ed276073a3754308ccdea7f5e975018a36ca
Vasiliy Khlopunov <khlopunov_v@goldapple.ru>
2024-02-26 23:18:53 +0500

    Добавили storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,9 @@
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:storybook/recommended"
   ],
   parserOptions: {
     ecmaVersion: 'latest'
