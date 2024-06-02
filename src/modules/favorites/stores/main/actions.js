eb2fdd6bd210058c68cfb6d0a41bfb419a1a4132
Vasiliy Khlopunov <vasia22012003@gmail.com>
2024-06-02 14:37:34 +0500

    Небольшие правки формы регистрации/авторизации

--- src/modules/favorites/stores/main/actions.js
+++ src/modules/favorites/stores/main/actions.js
@@ -49,7 +49,7 @@ export const actions = {
       const res = await getDoc(doc(db, 'users', userId))
 
       if (!res.exists()) {
-        throw new Error('Пользователь не найден')
+        return
       }
       
       for (const key in res.data().wishlist) {
